05e1e280e4c90b8372cf3ad07fbe953a544ad1c0
Maniramez <37369555+Maniramez@users.noreply.github.com>
2018-03-20 10:01:24 +0530

    Create largest no.py

---
+++ beginner level/largest no.py
@@ -0,0 +1,9 @@
+a=int(input("enter a value of a"))
+b=int(input("enter a value of b"))
+c=int(input("enter a value of c"))
+if(a>b and a>c):
+  print('a is large')
+elif(b>a and b>c):
+  print('b is large')
+else:
+  print('c is large')
