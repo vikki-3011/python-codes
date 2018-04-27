82e6a206d1ba8a90446d962ec58702d90ee5494d
Maniramez <37369555+Maniramez@users.noreply.github.com>
2018-04-27 12:08:43 +0530

    Create a b t i.py

---
+++ beginner level/a b t i.py
@@ -0,0 +1,13 @@
+lower = 150
+upper = 160
+for num in range(lower, upper + 1):
+   order = len(str(num))
+   sum = 0
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+   if num == sum:
+       print(num)
